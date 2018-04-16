8c1b02dbb067c73c71690b3b954cd2c553dc1ccd
tangshi <tangshi_job@163.com>
2018-04-16 19:09:01 +0800

    char01_20180416_01

---
+++ character1/example/em1.2.5gcd.rkt
@@ -0,0 +1,11 @@
+#lang racket
+
+(define (gcd a b)
+  (if(= b 0)
+     a
+     (gcd b
+          (remainder a
+                     b))))
+
+
+
---
+++ character1/example/em1.2.6FermatTest.rkt
@@ -0,0 +1,49 @@
+#lang racket
+(define (square x)
+  (* x x))
+
+(define (even? n)
+  (= (remainder n 2) 0))
+
+#|base的exp次幂对m取模|#
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+        ((even? exp) (remainder (square(expmod base
+                                               (/ exp 2)
+                                               m))
+                                m))
+        (else
+         (remainder (* base (expmod base (- exp 1) m))
+                    m))))
+
+
+(define (fermat-test n)
+  (define (try-it a)
+    (= (expmod a n n) a))
+  (try-it (+ 1 (random (- n 1)))))
+
+(define (fast-prime? n times)
+  (cond((= times 0) #t)
+       ((fermat-test n)(fast-prime? n (- times 1)))
+       (else #f)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
