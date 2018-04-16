89b78b6f3387a59ec8d438b6fd3e73b1e3dbe438
tangshi <tangshi_job@163.com>
2018-04-16 23:04:35 +0800

    char01_20180416_02

---
+++ character1/exercise/ex1.22.rkt
@@ -0,0 +1,35 @@
+#lang racket
+
+
+(define (prime? n)
+  )
+
+(define (timed-prime-test n)
+  (newline)
+  (display n)
+  (start-prime-test n (runtime)))
+
+(define (start-prime-test n start-time)
+  (if (prime? n)
+      (report-time (- (runtime) start-time))))
+
+(define (report-prime elapsed-time)
+  (display "***")
+  (display elapsed-time))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
