ea5a7d01c793a272233281b7b9297f9743dd0538
tangshi <tangshi_job@163.com>
2018-04-18 22:51:43 +0800

    char01_20180418_02

--- character1/exercise/ex1.29.rkt
+++ character1/exercise/ex1.29.rkt
@@ -1,10 +1,27 @@
 #lang racket
 
+;simpson-method
 
+(define (even? x)
+  (= 0 (remainder x 2)))
 
+(define (inc a)
+  (+ 1 a))
 
+(define (spsc m)
+  (cond ((= 1 m) 1)
+        ((even? m) 4)
+        (else 2)))
 
+(define (sum term a next b)
+  (if(> a b)
+     0
+     (+ (term a)
+        (sum term (next a) next b))))
 
+(define (simpson-term counter f a h)
+  (* (spsc counter)
+     (f (+ a (* counter h)))))
 
 
 
