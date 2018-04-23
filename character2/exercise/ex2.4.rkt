78a5c979b3df286ee4b0d5a9a1db6af5211ddc62
tangshi <tangshi_job@163.com>
2018-04-23 18:43:02 +0800

    chap02_20180423_01

---
+++ character2/exercise/ex2.4.rkt
@@ -0,0 +1,36 @@
+#lang racket
+
+(define (cons x y)
+  (lambda(m)(m x y)))
+
+(define (car z)
+  (z (lambda(x y) x)))
+
+(define (cdr z)
+  (z (lambda(x y) y)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
