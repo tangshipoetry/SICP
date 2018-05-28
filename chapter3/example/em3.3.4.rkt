8853f6f0a857cde74199ea3f7ac97bba94e86fe1
tangshi <tangshi_job@163.com>
2018-05-28 17:53:19 +0800

    1

--- chapter3/example/em3.3.4.rkt
+++ chapter3/example/em3.3.4.rkt
@@ -1,5 +1,41 @@
 #lang racket
 
+(define (call-each procedures)
+  (if(null? procedures)
+     'done
+     (begin ((car procedures))
+            (call-each (cdr procedures)))))
+
+(define (make-wire)
+  (let([signal-value 0][action-procedures '()])
+    ;信号设置，若与当前信号不一样,更改当前信号,并依次全部调用储存在线路中过程
+    (define (set-my-signal! new-value)
+      (if(not (= signal-value new-value))
+         (begin (set! signal-value new-value)
+                (call-each action-procedures))
+         'done))
+    ;线路内功能过程,在信号改变时依次调用。添加新功能时,立即运行这个新功能
+    (define (accept-action-procedure! proc)
+      (set! action-procedures
+            (cons proc action-procedures))
+      (proc))
+    ;按输入进行功能分配
+    (define (dispatch m)
+      (cond((eq? m 'get-signal) signal-value)
+           ((eq? m 'set-gignal!) set-my-signal!)
+           ((eq? m 'add-action!) accept-action-procedure!)
+           (else (error "Unknown operation: WIRE" m))))
+    dispatch))
+;小结:对于一般的wire,一般只会储存一个功能函数,这种情况下,在功能添加和信号修改时,过程调用
+
+
+(define (get-signal wire)(wire 'get-signal))
+(defeine (set-signal! wire new-value)
+  ((wire 'set-gignal) new-value))
+(define (add-action! wire action-procedure)
+  ((wire 'add-action!) action-procedure))
+
+;------------------------------------------------------------------------------------------------------------------------------------------------------
 (define (inverter input output)
   (define (inver-input)
     (let([new-value (logical-not (get-signal input))])
@@ -7,7 +43,6 @@
                    (lambda()(set-signal! output new-value)))))
   (add-action! input invert-input)
   'ok)
-
 (define (logical-not s)
   (cond((= 1 s) 0)
        ((= 0 s) 1)
@@ -17,15 +52,13 @@
 (define (and-gate a1 a2 output)
   (define (and-action-procedure)
     (let([new-value
-          (logical-and (set-signal! a1) (set-signal! a2))])
+          (logical-and (get-signal! a1) (get-signal! a2))])
       (after-delay
        and-gate-delay
        (lambda()(set-signal! out-put new-value)))))
   (add-action! a1 and-action-procedure)
   (add-action! a2 and-action-procedure)
   'ok)
-
-
 (define (logical-and s1 s2)
   (cond((and (= 1 s1)
              (= 1 s2)) 1)
@@ -38,6 +71,9 @@
        (else
         (error "invalid signal" s1 s2))))
 
+
+
+;----------------------------------------------------------------------------------------------------------------------------------------------------------
 (define (logical-or s1 s2)
   (cond((and (= 0 s1)
              (= 0 s2)) 0)
@@ -63,7 +99,7 @@
 
 
 
-;---------------------------------------------------------------------------------------
+;----------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
 
@@ -84,39 +120,6 @@
     (half-adder a s sum c2)
     (or-gate c1 c2 c-out)
     'ok))
-
-
-(define (make-wire)
-  (let([signal-value 0][action-procedures '()])
-    (define (set-my-signal! new-value)
-      (if(not (= signal-value new-value))
-         (begin (set! signal-value new-value)
-                (call-each action-procedures))
-         'done))
-    (define (accept-action-procedure! proc)
-      (set! action-procedures
-            (cons proc action-procedures))
-      (proc))
-    (define (dispatch m)
-      (cond((eq? m 'get-signal) signal-value)
-           ((eq? m 'set-gignal!) set-my-signal!)
-           ((eq? m 'add-action!) accept-action-procedure!)
-           (else (error "Unknown operation: WIRE" m))))))
-
-
-(define (call-each procedures)
-  (if(null? procedures)
-     'done
-     (begin ((car procedures))
-            (call-each (cdr procedures)))))
-
-
-(define (get-signal wire)(wire 'get-signal))
-(defeine (set-signal! wire new-value)
-  ((wire 'set-gignal) new-value))
-(define (add-action! wire action-procedure)
-  ((wire 'add-action!) action-procedure))
-
 
 
 (define (make-agenda))
