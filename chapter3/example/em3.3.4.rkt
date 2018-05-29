a88f96795e77a45572859047e250450d12e0e849
tangshi <tangshi_job@163.com>
2018-05-29 17:49:20 +0800

    1

--- chapter3/example/em3.3.4.rkt
+++ chapter3/example/em3.3.4.rkt
@@ -1,4 +1,100 @@
-#lang racket
+#lang sicp
+
+(define (make-queue) (cons '() '()))
+(define (front-ptr queue) (car queue))
+(define (rear-ptr queue) (cdr queue))
+(define (front-queue queue)
+  (if(empty-queue? queue)
+     (error "FRONT called with an empty queue" queue)
+     (car (front-ptr queue))))
+
+(define (set-front-ptr! queue item) (set-car! queue item))
+(define (set-rear-ptr! queue item) (set-cdr! queue item))
+(define (empty-queue? queue) (null? (front-ptr queue)))
+
+(define (insert-queue! queue item)
+  (let([new-pair (list item)])
+    (cond((empty-queue? queue)
+          (set-front-ptr! queue new-pair)
+          (set-rear-ptr! queue new-pair)
+          queue)
+         (else
+          (set-cdr! (rear-ptr queue) new-pair)
+          (set-rear-ptr! queue new-pair)
+          queue))))
+
+(define (delete-queue! queue)
+  (cond((empty-queue? queue)
+        (error "FRONT called with an empty queue" queue))
+       (else
+        (set-front-ptr! queue (cdr (front-ptr queue)))
+        queue)))
+
+(define (print-queue queue)
+  (car queue))
+
+;------------------------------------------------------------------------------------------------------------------------------------------------------
+(define (make-time-segment time queue)
+  (cons time queue))
+(define (segment-time s)(car s))
+(define (segment-queue s)(cdr s))
+
+(define (make-agenda)
+  (list 0))
+(define (current-time agenda)
+  (car agenda))
+(define (set-current-time! agenda time)
+  (set-car! agenda time))
+(define (segments agenda)
+  (cdr agenda))
+(define (set-segments! agenda segments)
+  (set-cdr! agenda segments))
+(define (first-segment agenda)
+  (car (segments agenda)))
+(define (rest-segments agenda)
+  (cdr (segments agenda)))
+;----------------------------------------------------------------------------------------------------------------------------------------------------------
+(define (empty-agenda? agenda)
+  (null? (segments agenda)))
+
+
+(define (add-to-agenda! time action agenda)
+  (define (belongs-before? segments)
+    (or (null? segments)
+        (< time (segment-time (car segments)))))
+  (define (make-new-time-segment time action)
+    (let([q (make-queue)])
+      (insert-queue! q action)
+      (make-time-segment time q)))
+  (define (add-to-segments! segments)
+    (if(= (segment-time (car segments)) time)
+       (insert-queue! (segment-time (car segments))
+                      action)
+       (let([rest (cdr segments)])
+         (if(belongs-before? rest)
+            (set-cdr! segments (cons (make-new-time-segment time action)
+                                     (cdr segments)))
+            (add-to-segments! rest)))))
+  (let([segments (segments agenda)])
+    (if(belongs-before? segments)
+       (set-segments! agenda (cons (make-new-time-segment time action)
+                                  segments))
+       (add-to-segments! segments))))
+
+(define (remove-first-agenda-item! agenda)
+  (let([q (segment-queue (first-segment agenda))])
+    (delete-queue! q)
+    (if(empty-queue? q)
+       (set-segments! agenda (rest-segments agenda)))))
+
+(define (first-agenda-item agenda)
+  (if (empty-agenda? agenda)
+      (error "Agenda is empty: FIRST-AGENDA-ITEM")
+      (let ((first-seg (first-segment agenda)))
+        (set-current-time! agenda
+                           (segment-time first-seg))
+        (front-queue (segment-queue first-seg)))))
+;----------------------------------------------------------------------------------------------------------------------------------------------------------
 
 (define (call-each procedures)
   (if(null? procedures)
@@ -30,14 +126,14 @@
 
 
 (define (get-signal wire)(wire 'get-signal))
-(defeine (set-signal! wire new-value)
+(define (set-signal! wire new-value)
   ((wire 'set-gignal) new-value))
 (define (add-action! wire action-procedure)
   ((wire 'add-action!) action-procedure))
 
 ;------------------------------------------------------------------------------------------------------------------------------------------------------
 (define (inverter input output)
-  (define (inver-input)
+  (define (invert-input)
     (let([new-value (logical-not (get-signal input))])
       (after-delay inverter-delay
                    (lambda()(set-signal! output new-value)))))
@@ -52,10 +148,10 @@
 (define (and-gate a1 a2 output)
   (define (and-action-procedure)
     (let([new-value
-          (logical-and (get-signal! a1) (get-signal! a2))])
+          (logical-and (get-signal a1) (get-signal a2))])
       (after-delay
        and-gate-delay
-       (lambda()(set-signal! out-put new-value)))))
+       (lambda()(set-signal! output new-value)))))
   (add-action! a1 and-action-procedure)
   (add-action! a2 and-action-procedure)
   'ok)
@@ -89,7 +185,7 @@
 (define (or-gate a1 a2 out-put)
   (define (or-action-procedure)
     (let([new-value
-          (logical-or (get-signal s1) (get-signal s2))])
+          (logical-or (get-signal a1) (get-signal a2))])
       (after-delay
        or-gate-delay
        (lambda()(set-signal! out-put new-value)))))
@@ -100,8 +196,6 @@
 
 
 ;----------------------------------------------------------------------------------------------------------------------------------------------------------
-
-
 
 (define (half-adder a b s c)
   (let([d (make-wire)]
@@ -121,15 +215,8 @@
     (or-gate c1 c2 c-out)
     'ok))
 
-
-(define (make-agenda))
-(define (empty-agenda? agenda))
-(define (first-agenda-item agenda))
-(define (remove-first-agenda-item! agenda))
-(define (add-to-agenda! time action agenda))
-(define (current-time agenda))
-
-(define (after-dalay delay action)
+;----------------------------------------------------------------------------------------------------------------------------------------------------------
+(define (after-delay delay action)
   (add-to-agenda! (+ delay (current-time the-agenda))
                   action
                   the-agenda))
@@ -161,12 +248,31 @@
 (define and-gate-delay 3)
 (define or-gate-delay 5)
 
-
+;----------------------------------------------------------------------------------------------------------------------------------------------------------
 
 (define input-1 (make-wire))
 (define input-2 (make-wire))
 (define sum (make-wire))
 (define carry (make-wire))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
