96a5bc07e26888df218d3587422f6dbe9e28722f
tangshi <tangshi_job@163.com>
2018-06-24 08:58:47 +0800

    1

--- chapter4/exercise/ex4.14.rkt
+++ chapter4/exercise/ex4.14.rkt
@@ -1,12 +1,13 @@
 #lang racket
 
 
-
-
-
-
-
-
+;网上的
+Eva's map can work, because he implemented it.
+But when installing map in the eval as a primitive procecdure,
+there is something wrong. for example:
+when eval expression '(map + (1 2) (3 4)),
+primitive procedure + is interpreted as '(application + env),
+so the expression is (apply map (list 'application + env) (list 1 2) (list 3 4))), it doesn't work.
 
 
 
