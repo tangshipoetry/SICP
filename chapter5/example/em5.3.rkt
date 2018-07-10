d529e451af58f5df8fc009e37c0d3645fc8f98a3
tangshi <tangshi_job@163.com>
2018-07-10 17:43:58 +0800

    1

--- chapter5/example/em5.3.rkt
+++ chapter5/example/em5.3.rkt
@@ -42,6 +42,92 @@
 (assign the-stack (const ()))
 
 
+begin-garbage-collection
+(assign free (const 0))
+(assign scan (const 0))
+(assign old (reg root))
+(assign relocate-continue (label reassign-root))
+(goto (label relocate-old-result-in-new))
+reassign-root
+(assign root (reg new))
+(goto (label gc-loop))
+
+gc-loop
+(test (op =) (reg scan) (reg free))
+(branch (label gc-flip))
+(assign old (op vector-ref) (reg new-cars) (reg scan))
+(assign relocate-continue (label update-car))
+(goto (label relocate-old-result-in-new))
+
+update-car
+(perform (op vector-set!)
+         (reg new-cars)
+         (reg scan)
+         (reg new))
+(assign old (op vector-ref) (reg new-cdrs) (reg scan))
+(assign relocate-continue (label update-cdr))
+(goto (label relocate-old-result-in-new))
+update-cdr
+(perform (op vector-set!)
+         (reg new-cdrs)
+         (reg scan)
+         (reg new))
+(assign scan (op +) (reg scan) (const 1))
+(goto (label gc-loop))
+
+relocate-old-result-in-new              ;将旧值移动到新的位置
+(test (op pointer-to-pair?) (reg old))
+(branch (label pair))
+(assign new (reg old))                  ;非序对,直接赋值
+(goto (reg relocate-continue))
+pair                                    ;序对操作
+(assign oldcr (op vector-ref) (reg the-cars) (reg old));oldcr设置为旧值的car
+(test (op broken-heart?) (reg oldcr))   ;判断内容是否已经移动
+(branch (label already-moved))
+(assign new (reg free))                 ;为序对分配新位置空间
+(assign free (op +) (reg free) (const 1));更新指针
+;; Copy the car and cdr to new memory.
+(perform (op vector-set!)
+         (reg new-cars) (reg new) (reg oldcr))
+(assign oldcr (op vector-ref) (reg the-cdrs) (reg old));oldcr设置为旧值的cdr
+(perform (op vector-set!)
+         (reg new-cdrs) (reg new) (reg oldcr))
+;; Construct the broken heart.
+(perform (op vector-set!)
+         (reg the-cars) (reg old) (const broken-heart))
+(perform
+ (op vector-set!) (reg the-cdrs) (reg old) (reg new))
+(goto (reg relocate-continue))
+already-moved
+(assign new (op vector-ref) (reg the-cdrs) (reg old))
+(goto (reg relocate-continue))
+
+gc-flip
+(assign temp (reg the-cdrs))
+(assign the-cdrs (reg new-cdrs))
+(assign new-cdrs (reg temp))
+(assign temp (reg the-cars))
+(assign the-cars (reg new-cars))
+(assign new-cars (reg temp))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
