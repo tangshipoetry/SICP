6073bfda911538bab274f48132eaf79ac36185e6
tangshi <tangshi_job@163.com>
2018-07-11 17:37:19 +0800

    1

--- chapter5/example/em5.5.rkt
+++ chapter5/example/em5.5.rkt
@@ -42,8 +42,28 @@ signal-error
         ⟨complete list of operations for eceval machine⟩))
 
 
-
-
+;compile过程
+(define (compile exp target linkage)
+  (cond ((self-evaluating? exp)
+         (compile-self-evaluating exp target linkage))
+        ((quoted? exp) (compile-quoted exp target linkage))
+        ((variable? exp)
+         (compile-variable exp target linkage))
+        ((assignment? exp)
+         (compile-assignment exp target linkage))
+        ((definition? exp)
+         (compile-definition exp target linkage))
+        ((if? exp) (compile-if exp target linkage))
+        ((lambda? exp) (compile-lambda exp target linkage))
+        ((begin? exp)
+         (compile-sequence
+          (begin-actions exp) target linkage))
+        ((cond? exp)
+         (compile (cond->if exp) target linkage))
+        ((application? exp)
+         (compile-application exp target linkage))
+        (else
+         (error "Unknown expression type: COMPILE" exp))))
 
 
 
